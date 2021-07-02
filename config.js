7d72f4c1414ba7035be02cf23c6db31bd70a7dfa
Ng2006 <86716998+Ng2006@users.noreply.github.com>
2021-07-03 00:26:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Yêu anh mất rồi Trí à <33',
     btnNo: 'Không, Anh mơ à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anhhhhhh<3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Anh tuyệt vời nhất trên đời  <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
