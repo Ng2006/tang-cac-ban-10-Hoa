8dc5fbebafcbb96ca54664786727ef0001de4b74
Ng2006 <86716998+Ng2006@users.noreply.github.com>
2021-07-03 18:16:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
-    introTitle: 'Thảo dzân à!',
-    introDesc: `Thiệt ra tao ko muốn nhắc đâu, nhưng mà ... 😔 khi nào mày trả tao 10 tỷ đây. 
-                 Mày nợ tao 4 năm rồi`,
-    btnIntro: 'tao định ko trả',
-    title: 'Phải chăng em đã định bùm ngay từ cái lúc mượn tiền 🙂',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy số tiền ấy ',
-    btnYes: 'Em sẽ trả , nếu ko em sẽ đi tu ',
-    btnNo: 'Không, Anh trai à, Có chết em cũng không trả',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại mượn tiền Anh',
-    btnReply: 'Gửi cho anh ',
-    reply: 'Bởi vì Anh có bồ cũ xinh quá, em ghen tỵ',
-    mess: 'Anh biết mà 🥰. Lo trả tiền anh đi',
-    messDesc: 'Ngày mai 7h anh qua nhà lấy trước 5 tỷ.',
+    introTitle: 'Thảo dzân à!Tao Tường đây này😔',
+    introDesc: `Thiệt ra tao muốn nói vơi mày 
+                Một câu là T..T..Tao thích mày`,
+    btnIntro: 'Ơ kìa! tao cũng thích mày😔',
+    title: 'Phải chăng tao đã yêu ngay từ cái nhìn đầu tiên 😚',
+    desc: 'Phải chăng tao đã say ngay từ lúc nụ cười ấy ',
+    btnYes: 'Em sẽ yêu mỗi Anh thôi ',
+    btnNo: 'Không, Anh trai à',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Anh',
+    btnReply: 'Gửi cho anh nhéee ',
+    reply: 'Bởi vì Em thích người giỏi hoá ',
+    mess: 'Anh biết mà 🥰.',
+    messDesc: 'Tối nay 7h anh qua nhà bày em học hoá nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100009528825607'
+    messLink: 'https://www.facebook.com/profile.php?id=100009605621077'
 }
