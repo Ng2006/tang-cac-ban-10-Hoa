8118021cf1799f4d51c55d8cfb204fc33f4fe2d1
Ng2006 <86716998+Ng2006@users.noreply.github.com>
2021-10-17 23:53:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 const CONFIG = {
     introTitle: 'Chào các bạn nữ lớp 10 Hoá và cô Linh💗',
-    introDesc: `Thiệt ra hôm nay tụi mình làm cái này để dành 
-                Tặng cho các bạn nữ nhân ngày 20/10 sắp tới`,
+    introDesc: `Thiệt ra hôm nay tụi mình 
+    làm cái này để dành 
+    Tặng cho các bạn nữ nhân ngày 20/10 sắp tới`,
     btnIntro: 'Các bạn bấm vào đây đi!! HiHi🤭',
     title: 'Ngày 20/10 chúc một nửa thế giới luôn thành công trong cuộc sống, 
             Học tập, duyên dáng và xinh đẹp trong mắt một nửa thế giới còn lại nhé.',
