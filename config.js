c441e87dbb9c3ccd2049a565e9b0d9ed23697f3a
Ng2006 <86716998+Ng2006@users.noreply.github.com>
2021-10-18 21:28:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: '你好👋 các bạn nữ lớp 10 Hoá và cô Linh💗!',
+    introTitle: 'Chào các bạn nữ lớp 10 Hoá và cô Linh💗!',
     introDesc: `Thật ra hôm nay
-    Tụi mình làm cái này 
+    mình làm cái này 
     Để dành cho mấy bạn
     Nhân ngày 20/10`,
     btnIntro: 'bấm vào đây ',
     title: 'Chúc nửa thế giới kia luôn thành công trong cuộc sống nháa 😙',
     desc: 'Các bạn có củm động hông nèo ',
-    btnYes: 'Coá nhaa, mãi yêu<333',
+    btnYes: 'Coá nhaa <333',
     btnNo: 'Hông nha, dở ẹc :3',
     question:'Tại sao các bạn lại thích món quà này vậy <3',
     btnReply: 'Gửi cho mình nhé <3',
-    reply: 'Mấy bạn thật tuyệt dòi   <33333333',
-    mess: 'Mình biết mà 😆. 谢谢 😘😘',
-    messDesc: 'Hẹn mấy bạn ngày 25/10 nhé 再见👋.',
+    reply: 'Bạn thật tuyệt dời   <33333333',
+    mess: 'Mình biết mà 😆. Cảm ơn mấy bạn 😘😘',
+    messDesc: 'Hẹn mấy bạn ngày 25/10 nhé .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?'
 }
